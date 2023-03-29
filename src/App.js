68de61e3fa6d335db614d27c42394b60504ef8a0
Anastasia Soloveva <solanastacia402@gmail.com>
2023-03-29 22:39:01 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios';
 import './App.css';
+import Chat from './components/Chat';
 import JoinBlock from './components/JoinBlock';
+import reducer from './reducer';
 import socket from './socket';
 
+
 function App() {
+
+	const [state, dispatch] = useReducer(reducer, {
+		joined: false,
+		roomId: null,
+		userName: null,
+		users: [],
+		messages: []
+	});
+
+	const onlogin = async (obj) => {
+		dispatch({
+			type: 'JOINED',
+			payload: obj
+		});
+		socket.emit('ROOM:JOIN', obj);
+		const { data } = await axios.get(`/rooms/${obj.roomId}`);
+		dispatch({
+			type: 'SET_DATA',
+			payload: data,
+		});
+	};
+
+	const setUsers = (users) => {
+		dispatch({
+			type: 'SET_USERS',
+			payload: users
+		});
+	};
+
+	const addMessage = (message) => {
+		dispatch({
+			type: 'NEW_MESSAGE',
+			payload: message
+		});
+	};
+
+
+	useEffect(() => {
+		socket.on('ROOM:SET_USERS', setUsers);
+		socket.on('ROOM:NEW_MESSAGE', addMessage);
+	}, []);
+
+	window.socket = socket;
+
 	return (
 		<div className="wrapper">
-			<JoinBlock />
+			{!state.joined ? <JoinBlock onLogin={onlogin} /> : <Chat {...state} onAddMessage={addMessage} />}
 		</div>
 	);
 }
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import socket from '../socket';
 
-const JoinBlock = () => {
+const JoinBlock = ({ onLogin }) => {
+	const [roomId, setRoomId] = useState('');
+	const [userName, setUserName] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+
+	const onEnter = async () => {
+		if (!roomId || !userName) {
+			return alert('Неверные данные');
+		}
+		const obj = {
+			roomId,
+			userName
+		};
+		setIsLoading(true);
+		await axios.post('/rooms', obj);
+		onLogin(obj);
+	}
+
 	return (
 		<div className="join-block">
-			<input type="text" placeholder="Room ID" />
-			<input type="text" placeholder="Your name" />
-			<button className="btn btn-success">ВОЙТИ</button>
+			<input
+				type="text"
+				placeholder="Room ID"
+				value={roomId}
+				onChange={(e) => setRoomId(e.target.value)}
+			/>
+			<input
+				type="text"
+				placeholder="Your name"
+				value={userName}
+				onChange={(e) => setUserName(e.target.value)}
+			/>
+			<button disabled={isLoading} onClick={onEnter} className="btn btn-success">
+				{isLoading ? 'ВХОД...' : 'ВОЙТИ'}
+			</button>
 		</div>
 	)
 };
